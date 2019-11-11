77a679fe50943b639e639c5600eca83a9f9ef248
chambrig <54903181+chambrig@users.noreply.github.com>
2019-11-11 11:44:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 
 const app = express();
-const port = 9000;
+const port = 80;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
